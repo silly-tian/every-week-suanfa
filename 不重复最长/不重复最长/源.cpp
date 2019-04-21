7a1d31cb41c17a4e39f15760d65fe75eecefb3c9
silly-tian <1261557295@qq.com>
2019-04-21 16:45:27 +0800

    2.0

---
+++ 不重复最长/不重复最长/源.cpp
@@ -0,0 +1,35 @@
+//本周算法题目：1.给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	int count_1 = 0;
+	int count_2 = 0;
+	int i, j;
+	int n = 0;
+	char s[100];
+	printf("请输入一个字符串:");
+	scanf_s("%s", s, 100);
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		for (j = n; j < i; j++)
+		{
+			if (s[i] == s[j])
+			{
+				count_1 = (i - n);
+				n = i;
+				if (count_1 > count_2)
+				{
+					count_2 = count_1;
+				}
+				break;
+			}
+		}
+	}
+	printf("最长为:%d", count_2);
+
+	system("pause");
+	return 0;
+}
